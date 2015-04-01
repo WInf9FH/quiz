48cec57f7208b803434afef5bd0d9c8e6f315fd2
WInf9KR <WInf9KR@users.noreply.github.com>
2015-04-01 16:13:22 +0200

    Update playerListProcessors.py
    Kleine Verbesserungen, die die Funktionalität ein wenig besser absichern.

--- playerListProcessors.py
+++ playerListProcessors.py
@@ -1,17 +1,17 @@
 import json
 
-SL=[]
-
 def players():
+    SL = []
     AnzSp=int(input("Wie viele Spieler spielen mit?""\t"))
     for i in range(1,AnzSp+1):
         spieler = "Name von Spieler "+ str(i)+":"
         SpName=input(spieler)
         SL.append([SpName,0])
-        savePlayerList()
+        savePlayerList(SL)
     return AnzSp
 
 def give_player_key():
+    SL = []
     AnzSp=int(input("Wie viele Spieler spielen mit?""\t"))
     for i in range(1,AnzSp+1):
         spieler = "Name von Spieler "+ str(i)+":"
@@ -19,13 +19,14 @@ def give_player_key():
         key = "Taste von Spieler "+ str(i)+":"
         SpKey = input(key)
         SL.append([SpName,0,SpKey])
-        savePlayerList()
+        savePlayerList(SL)
 
-def savePlayerList():
+def savePlayerList(liste):
     out_file = open('playerList.txt', 'wt')
-    out_file.write(json.dumps(SL))
+    out_file.write(json.dumps(liste))
     out_file.close()
 
+# emptyPlayerList wird im Programm nicht genutzt
 def emptyPlayerList():
     empty = "Hier gibt es derzeit nichts. Starte den Quiz, dann schon!"
     out_file = open('playerList.txt', 'wt')
