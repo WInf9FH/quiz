c0b64dd914290fe99ef030b47a1ab206cb29f954
WInf9FH <Frank.Hellmuth@wobogym.de>
2015-05-04 22:54:00 +0200

    Umlaut in Variablennamen in "ae" umgewandelt
    Umlaute in Variablennamen gehen *eigentlich* zwar in Python, aber eben nur eigentlich ... und führen unter Umständen doch zu schwer zu findenden Fehlern: Dann läuft das Programm auf einmal unter MacOS oder Linux nicht mehr.

--- PrintTexts.py
+++ PrintTexts.py
@@ -2,6 +2,6 @@ def print_question(L,r):
     print(L[r][0])
 
 def print_answers(L,r):
-    länge = len(L[r][1])
-    for i in range(0,länge):
+    laenge = len(L[r][1])
+    for i in range(0,laenge):
         print(L[r][1][i])
