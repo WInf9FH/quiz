f0916865e579e51598d1b4a0ca8310c7247fcf25
Swag <bmxstreet@gmx.net>
2015-05-04 09:09:14 +0200

    Kein Abbruch bei Falscher Taste
    Das Programm wird bei einer falschen Tasten eingabe nicht mehr abgebrochen.

--- Quiz.py
+++ Quiz.py
@@ -23,7 +23,7 @@ def refreshPlayerList(): #Funktioniert anscheinend nicht!
 print("Verfügbare Spielvarianten:")
 print("1 - Jeder Spieler gibt eine Antwort ab.")
 print("2 - Jeder Spieler bekommt eine Taste zugewiesen, drückt er diese,\nist er der einzige der die Frage beantworten darf.")
-Spielart = input("Welche möchtest du nutzen?")
+Spielart = input("Welche möchtest du nutzen? ")
 
 
 # Spielvariante 1
@@ -38,7 +38,7 @@ if int(Spielart) == 1:
           print_answers(Quiz, o)
           for u in range(0,y):
               print(SL[u][0],"ist an der Reihe") 
-              x=input("Antwort:")
+              x=input("Antwort: ")
               if correct_answer(Quiz,o,x):
                   SL[u][1]=SL[u][1]+1
                   savePlayerList(SL)
@@ -61,10 +61,13 @@ else:
     for o in range (0,f):
         print_question(Quiz,o)
         print_answers(Quiz, o)
-        PressedKey = input("Wenn du die Antwort weißt, drücke deine Taste!")
-        getPlayer = getPlayerByKey(PressedKey)
+        PressedKey = input("Wenn du die Antwort weißt, drücke deine Taste! ")
+        getPlayer = -1
+        while getPlayer == -1:
+            PressedKey = input("Diese Taste ist falsch. Noch einmal: ")
+            getPlayer = getPlayerByKey(PressedKey)
         print(SL[getPlayer][0],"ist an der Reihe.")
-        x = input("Welche Antwort ist richtig?")
+        x = input("Welche Antwort ist richtig? ")
         punkte_rechnen(Quiz,o,x, PressedKey)
         result_answers(o,Quiz,SL)
     in_file = open ("playerList.txt","rt")
