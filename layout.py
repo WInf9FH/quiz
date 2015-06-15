8a32f2d64a4e3ac3421cc0054b76ae5721e3a3a4
WInf9FH <Frank.Hellmuth@wobogym.de>
2015-06-15 08:44:40 +0200

    Create layout.py
    Das Layout des Quiz, alle Angaben in  Prozent

---
+++ layout.py
@@ -0,0 +1,29 @@
+# Raender
+RANDL = 7
+RANDR = RANDL
+RANDO = 12
+RANDU = RANDO
+
+# Fragekasten
+FRAGEH = 30
+FRAGEB = 86
+
+# Antwortkasten
+ANTWORTH = 15
+ANTWORTB = 17
+
+# Spielerkasten
+SPIELERH = ANTWORTH
+SPIELERB = ANTWORTB
+
+# Abstand Frage <-> Antworten
+ABSTANDFA = 12
+
+# Abstand Antwort <-> Antwort
+ABSTANDAA = 4
+
+# Abstand Antworten <-> Spieler
+ABSTANDAS = 7
+
+# Abstand Kaestchenhoehe
+ABSTANDKH = 4
